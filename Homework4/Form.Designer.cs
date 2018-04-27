225e675f7792b9886a6b14e69a4f1bbbc7a8ec66
БаймуханбетоваА <@A-106-01>
2018-04-27 19:44:11 +0600

    Добавьте файлы проекта.

---
+++ Homework4/Form.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Homework4
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView = new System.Windows.Forms.ListView();
+            this.processNameColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.idColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.handleColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.startTimeColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.startInfoColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.processNameColumn,
+            this.idColumn,
+            this.handleColumn,
+            this.startTimeColumn,
+            this.startInfoColumn});
+            this.listView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView.Location = new System.Drawing.Point(13, 13);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(664, 429);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // processNameColumn
+            // 
+            this.processNameColumn.Text = "ProcessName";
+            this.processNameColumn.Width = 130;
+            // 
+            // idColumn
+            // 
+            this.idColumn.Text = "ID";
+            this.idColumn.Width = 132;
+            // 
+            // handleColumn
+            // 
+            this.handleColumn.Text = "Handle";
+            this.handleColumn.Width = 126;
+            // 
+            // startTimeColumn
+            // 
+            this.startTimeColumn.Text = "StartTime";
+            this.startTimeColumn.Width = 142;
+            // 
+            // startInfoColumn
+            // 
+            this.startInfoColumn.Text = "StartInfo";
+            this.startInfoColumn.Width = 130;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 454);
+            this.Controls.Add(this.listView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form";
+            this.Text = "Processes";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader processNameColumn;
+        private System.Windows.Forms.ColumnHeader idColumn;
+        private System.Windows.Forms.ColumnHeader handleColumn;
+        private System.Windows.Forms.ColumnHeader startTimeColumn;
+        private System.Windows.Forms.ColumnHeader startInfoColumn;
+    }
+}
+
---
+++ Homework4/Form.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework4
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private Thread thread;
+        private Process[] processes;
+        public Form()
+        {
+            InitializeComponent();
+            thread = new Thread(ShowProcessInformation);
+            thread.Start();
+        }
+
+        private void ShowProcessInformation()
+        {
+            while (true)
+            {
+                try
+                {
+                    processes = Process.GetProcesses();
+                    foreach (Process process in processes)
+                    {
+                        listView.Items.Add(
+                            new ListViewItem(
+                                new string[] {
+                            $"{process.ProcessName}",
+                                $"{process.Id}",
+                                $"{process.HandleCount}",
+                                $"{process.StartTime}",
+                                $"{process.StartInfo}"
+                                }));
+                    }
+                }
+                catch(Exception ex) { MessageBox.Show(ex.Message); }
+                Thread.Sleep(5000);
+                listView.Refresh();
+            }
+        }
+    }
+}
